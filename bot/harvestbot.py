13ae0c7e534a027b6cabd143896736e02fc5118e
ElectricNinja315 <suzac314@gmail.com>
2017-12-25 22:24:33 -0800

    add whitespace, comments
    adjusted formatting of code to better fit PEP8 standards and clarify code block purposes

--- bot/harvestbot.py
+++ bot/harvestbot.py
@@ -3,7 +3,7 @@ import discord
 from bot.constants import *
 from bot.safety import token
 
-client = discord.Client()
+client = discord.Client()   # initialize client object
 
 
 @client.event
@@ -13,8 +13,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # help function
     if message.content == "!big noob help":
         await client.send_message(message.channel, github)
+
+    # meme functions
     elif message.content == "!meme music":
         await client.send_message(message.channel, "!music " + meme_music)
 
