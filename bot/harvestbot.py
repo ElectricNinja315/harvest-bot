2191bf7468ddbbbbb489a800549d4a580973e306
ElectricNinja315 <suzac314@gmail.com>
2018-01-05 10:50:41 -0800

    new error message if nothing is copied

--- bot/harvestbot.py
+++ bot/harvestbot.py
@@ -23,7 +23,7 @@ async def on_message(message):
 
     elif message.content.startswith("!copy"):
         to_copy = message.content[6:]   # text to copy
-        name = message.author.id  # username
+        name = message.author.id  # id
 
         fo = open(r'userstexts/%s.txt' % name, "w")  # init file object
         fo.write(to_copy)   # write text to copy to document
@@ -33,7 +33,11 @@ async def on_message(message):
 
     elif message.content == "!paste":
         name = message.author.id
-        fo = open(r'userstexts/%s.txt' % name, "r")  # init file object
-        await client.send_message(message.channel, fo.read())   # send the user's text
+        try:    # if the user has already copied something
+            fo = open(r'userstexts/%s.txt' % name, "r")  # init file object
+            await client.send_message(message.channel, fo.read())   # send the user's text
+            fo.close()
+        except FileNotFoundError:   # if nothing is copied
+            await client.send_message(message.channel, "You do not currently have anything copied.")
 
 client.run(token)
